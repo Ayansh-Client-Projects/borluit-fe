8e20f6281094092f5663f89d2e91ad017ddad8eb
Anubhav Adarsh <anubhav.adarsh9@gmail.com>
2024-06-10 15:27:21 +0530

    upload assets

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
+import Navbar from "@components/Navbar";
 import HomePage from "@page/home";
+import OurProductsPage from "@page/OurProducts";
+import AboutUsPage from "@page/about-us";
+import MissionVisionPage from "@page/mission-vision";
 
 const App = () => {
-  return <HomePage />;
+  return (
+    <>
+      <Navbar />
+      <HomePage />
+      <OurProductsPage />
+      <AboutUsPage />
+      <MissionVisionPage />
+    </>
+  );
 };
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,13 @@ export default {
   theme: {
 		fontFamily: {
 			'display': ['"Clash Display"', 'sans-serif'],
-			'satoshi': ['Satoshi', 'sans-serif']
+			'satoshi': ['Satoshi', 'sans-serif'],
+			'palanquin':['Palanquin Dark', 'sans-serif']
 		},
     extend: {
 			backgroundImage: {
-				'hero': "url('/src/assets/img/image-hero.png')"
+				'hero': "url('/src/assets/img/image-hero.png')",
+				'aboutUs': "url('/src/assets/img/tea-garden.png')"
 			},
 			colors: {
 				'canopy': '#55634B',
