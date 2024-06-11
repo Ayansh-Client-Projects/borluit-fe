886a03b5b78e2debda22d4299e4d2a6148eaa98a
Anubhav Adarsh <anubhav.adarsh9@gmail.com>
2024-06-11 17:37:16 +0530

    fix carousel not visible on large device - due to width

--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -8,8 +8,13 @@ const responsive = {
     items: 2,
     slidesToSlide: 2, // optional, default to 1.
   },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 2,
+    slidesToSlide: 2, // optional, default to 1.
+  },
   mobile: {
-    breakpoint: { max: 1024, min: 0 },
+    breakpoint: { max: 464, min: 0 },
     items: 1,
     slidesToSlide: 1, // optional, default to 1.
   },
@@ -24,8 +29,7 @@ const MyComp = () => {
       swipeable={true}
       draggable={true}
       showDots={true}
-      removeArrowOnDeviceType={["tablet", "mobile"]}
-      partialVisible={false}
+      removeArrowOnDeviceType={["mobile"]}
       dotListClass={styles["custom-dot-list-style"]}
     >
       {list.map((_, idx) => (
--- src/components/Review/index.tsx
+++ src/components/Review/index.tsx
@@ -5,7 +5,7 @@ const Review = () => {
   return (
     <section className="bg-[#F0F0F0] px-6 pt-16 lg:flex lg:flex-col lg:items-center lg:p-32">
       <Header title="What our customers are saying" />
-      <div className="pt-8 lg:max-w-[1200px]">
+      <div className="pt-8 lg:w-[1200px]">
         <MyComp />
       </div>
     </section>
